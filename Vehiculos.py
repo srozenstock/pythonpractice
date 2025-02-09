c50043a42bb1a6f8cb53c6b74a2432318df8517d
simon <a@a.com>
2025-02-09 16:33:22 +0200

    initial load

---
+++ Vehiculos.py
@@ -0,0 +1,21 @@
+from abc import ABC , abstractmethod
+import json
+
+class Vehiculo (ABC):
+    def __init__(self, placa, marca, modelo, combustible):
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.combustible = combustible
+
+    @abstractmethod
+    def estado(self):
+        pass
+
+class Moto(Vehiculo):
+    def __init__(self, placa, marca, modelo, combustible, estilo):
+        super().__init__(placa, marca, modelo, combustible)
+        self.estilo = estilo
+    
+    def estado(self):
+        return ""
