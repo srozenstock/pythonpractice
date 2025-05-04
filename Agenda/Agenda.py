8311c67f1027e495e8ac48b0023a94b01a065079
simon <a@a.com>
2025-05-04 17:28:00 +0300

    metodos de carga, agregar y mostrar, prueba

--- Agenda/Agenda.py
+++ Agenda/Agenda.py
@@ -1,6 +1,8 @@
 # Abril 27 2025
 # Registro de contactos guardado en archivos CSV
 
+import csv 
+
 class Contacto():
     def __init__(self,nombre,telefono,correo,pais):
         self.__nombre = nombre
@@ -52,6 +54,51 @@ class Contacto():
         else:
             self.__pais = value
 
+    
 
+def guardar_contacto(contacto,archivo):
+    with open(archivo,mode="w",newline="",encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Nombre","Telefono","Correo","Pais"])
+        for c in contacto:
+            writer.writerow([c._nombre,c._telefono,c._correo,c._pais])
+
+def cargar_contacto(archivo):
+    contactos = []
+    try: 
+        with open(archivo,mode="r",encoding="utf-8") as file:
+            reader = csv.DictWriter(file)
+            for r in reader:
+                contacto = Contacto(r["Nombre"],r["Telefono"],r["Correo"],r["Pais"])
+                contactos.append(contacto)
+    except FileNotFoundError:
+        print("El archivo: {archivo} no fue encontrado")
+    return contactos
+
+def agregar_contacto(contactos):
+    print("Informacion del contacto")
+    nombre = input("Nombre: ")
+    telefono = input("Telefono: ")
+    correo = input("Correo: ")
+    pais = input("Pais: ")
+
+    contacto = Contacto(nombre,telefono,correo,pais)
+    contactos.append(contacto)
+
+def mostrar_contacto(contactos):
+    if len(contactos) > 0:
+        for idx, contacto in enumerate(contactos, 1):
+            print(f"{idx}. {contacto._nombre}")
+    else:
+        print("Agenda Vacia")
+
+#Prueba
+
+nuevo = []
+agregar_contacto(nuevo)
+agregar_contacto(nuevo)
+mostrar_contacto(nuevo)
+        
+    
 
 
